89c5204c8056eb7b844d11cb5811ea03954c9840
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-04 22:40:55 +1000

    Add doctrine fixtures bundle

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ else
    php app/console doctrine:database:drop --force
    php app/console doctrine:database:create
    php app/console doctrine:schema:create
-   php app/console doctrine:fixtures:load
+   php app/console doctrine:fixtures:load --fixtures=SlashStudio/AppBundle/DataFixtures/ORM
 fi
 
 php app/console assets:install web --symlink
