47f3f69c1c9d7a9a44df732bc72187f5159bfbd8
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-06 22:08:45 +1000

    Add page with products list

--- src/SlashStudio/AppBundle/Controller/AppController.php
+++ src/SlashStudio/AppBundle/Controller/AppController.php
@@ -9,9 +9,18 @@ class AppController extends Controller
     public function indexAction()
     {
         $manager = $this->getDoctrine()->getManager();
+
         return $this->render('SlashStudioAppBundle:App:index.html.twig', [
             'players' => $manager->getRepository('SlashStudioAppBundle:Player')->findAll(),
             'products' => $manager->getRepository('SlashStudioAppBundle:Product')->getForMainPage(),
+        ]);
+    }
+
+    public function productsAction()
+    {
+
+        return $this->render('SlashStudioAppBundle:App:products.html.twig', [
+             'products' => $this->getDoctrine()->getManager()->getRepository('SlashStudioAppBundle:Product')->findBy([], ['name' => 'ASC']),
         ]);
     }
 }
