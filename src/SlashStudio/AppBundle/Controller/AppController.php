dee26dd0b4e65200ea643d27953faa1a250ce108
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-07 12:19:56 +1000

    Extract products code from App to Product controller

--- src/SlashStudio/AppBundle/Controller/AppController.php
+++ src/SlashStudio/AppBundle/Controller/AppController.php
@@ -15,23 +15,4 @@ class AppController extends Controller
             'products' => $manager->getRepository('SlashStudioAppBundle:Product')->getProductsForMainPage(),
         ]);
     }
-
-    public function productsAction()
-    {
-        return $this->render('SlashStudioAppBundle:App:products.html.twig', [
-             'products' => $this->getDoctrine()->getManager()->getRepository('SlashStudioAppBundle:Product')->findBy([], ['name' => 'ASC']),
-        ]);
-    }
-
-    public function productShowAction($id)
-    {
-        $product = $this->getDoctrine()->getManager()->getRepository('SlashStudioAppBundle:Product')->getProductInfo($id);
-        if (empty($product)) {
-            throw $this->createNotFoundException($this->get('translator')->trans('product.not_found'));
-        }
-
-        return $this->render('SlashStudioAppBundle:App:product_show.html.twig', [
-            'product' => $product,
-        ]);
-    }
 }
---
+++ src/SlashStudio/AppBundle/Controller/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SlashStudio\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProductController extends Controller
+{
+    public function listAction()
+    {
+        return $this->render('SlashStudioAppBundle:Product:list.html.twig', [
+             'products' => $this->getDoctrine()->getManager()->getRepository('SlashStudioAppBundle:Product')->findBy([], ['name' => 'ASC']),
+        ]);
+    }
+
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()->getManager()->getRepository('SlashStudioAppBundle:Product')->getProductInfo($id);
+        if (empty($product)) {
+            throw $this->createNotFoundException($this->get('translator')->trans('product.not_found'));
+        }
+
+        return $this->render('SlashStudioAppBundle:Product:show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
