5bc37a41a158dcf4fe2ba1d50014347b15e96e3f
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-07 14:20:40 +1000

    Add controllers, template and translations for media menu route

---
+++ src/SlashStudio/AppBundle/Controller/MediaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SlashStudio\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MediaController extends Controller
+{
+    public function photoAction()
+    {
+        return $this->render('SlashStudioAppBundle:Media:photo.html.twig', [
+
+        ]);
+    }
+
+    public function videoAction()
+    {
+        return $this->render('SlashStudioAppBundle:Media:video.html.twig', [
+        ]);
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('SlashStudioAppBundle:Media:about.html.twig', [
+
+        ]);
+    }
+}
