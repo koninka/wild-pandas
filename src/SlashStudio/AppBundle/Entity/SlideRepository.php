c29c099770ed7e223adc729479552de09cc9a18a
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-14 18:26:34 +1000

    Убираю гидрацию слайдов

--- src/SlashStudio/AppBundle/Entity/SlideRepository.php
+++ src/SlashStudio/AppBundle/Entity/SlideRepository.php
@@ -4,12 +4,13 @@ namespace SlashStudio\AppBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
 
+
 class SlideRepository extends EntityRepository
 {
     public function getSlides()
     {
         return $this->getEntityManager()
             ->createQuery('SELECT s, i FROM SlashStudioAppBundle:Slide s LEFT JOIN s.image i')
-            ->getResult();
+            ->getArrayResult();
     }
 }
