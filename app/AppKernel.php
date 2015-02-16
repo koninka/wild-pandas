7e15cad3d8e8eb20ec05200c41377e2fe0269765
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-16 22:06:33 +1000

    Add a2lix form field type for translation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,6 +32,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+            new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
