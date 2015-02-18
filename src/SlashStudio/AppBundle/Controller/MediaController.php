7be8830d39eeb75f9b9d8a79c7c6530da208a52a
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-18 21:14:55 +1000

    Добавляю пагинацию на странице медиа постов

--- src/SlashStudio/AppBundle/Controller/MediaController.php
+++ src/SlashStudio/AppBundle/Controller/MediaController.php
@@ -3,9 +3,12 @@
 namespace SlashStudio\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MediaController extends Controller
 {
+    const MEDIA_POSTS_PER_PAGE = 9;
+
     public function photoAction()
     {
         return $this->render('SlashStudioAppBundle:Media:photo.html.twig', [
@@ -19,10 +22,16 @@ class MediaController extends Controller
         ]);
     }
 
-    public function aboutAction()
+    public function aboutAction(Request $request)
     {
+        $pagination = $this->get('knp_paginator')->paginate(
+            $this->getDoctrine()->getManager()->getRepository('SlashStudioAppBundle:MediaPost')->getMediaQuery(),
+            $request->query->get('page', 1),
+            static::MEDIA_POSTS_PER_PAGE
+        );
+
         return $this->render('SlashStudioAppBundle:Media:about.html.twig', [
-            'posts' => $this->getDoctrine()->getManager()->getRepository('SlashStudioAppBundle:MediaPost')->getMedia()
+            'pagination' => $pagination,
         ]);
     }
 }
--- src/SlashStudio/AppBundle/Entity/MediaPostRepository.php
+++ src/SlashStudio/AppBundle/Entity/MediaPostRepository.php
@@ -6,11 +6,10 @@ use Doctrine\ORM\EntityRepository;
 
 class MediaPostRepository extends EntityRepository
 {
-    public function getMedia()
+    public function getMediaQuery()
     {
         return $this->getEntityManager()
-            ->createQuery('SELECT m, i, t FROM SlashStudioAppBundle:MediaPost m LEFT JOIN m.image i LEFT JOIN m.translations t ORDER BY m.createdAt DESC')
-            ->getResult();
+            ->createQuery('SELECT m, i, t FROM SlashStudioAppBundle:MediaPost m LEFT JOIN m.image i LEFT JOIN m.translations t ORDER BY m.createdAt DESC');
 
     }
 }
