f51dc96e7330c6d08faaf5859ce424037bcd5420
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-18 21:09:37 +1000

    Добавляю пагинацию на страницу черлидеров

--- src/SlashStudio/AppBundle/Controller/CheerleaderController.php
+++ src/SlashStudio/AppBundle/Controller/CheerleaderController.php
@@ -3,16 +3,24 @@
 namespace SlashStudio\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CheerleaderController extends Controller
 {
-    public function listAction()
+    const CHEERLEADERS_PER_PAGE = 3;
+
+    public function listAction(Request $request)
     {
         $manager = $this->getDoctrine()->getManager();
+        $pagination = $this->get('knp_paginator')->paginate(
+            $manager->getRepository('SlashStudioAppBundle:Cheerleader')->getAllQB(),
+            $request->query->get('page', 1),
+            static::CHEERLEADERS_PER_PAGE
+        );
 
         return $this->render('SlashStudioAppBundle:Cheerleader:list.html.twig', [
             'team' => $manager->getRepository('SlashStudioAppBundle:CheerleaderTeam')->getInfo(),
-            'cheerleaders' => $manager->getRepository('SlashStudioAppBundle:Cheerleader')->getAll(),
+            'pagination' => $pagination,
         ]);
     }
 }
--- src/SlashStudio/AppBundle/Entity/CheerleaderRepository.php
+++ src/SlashStudio/AppBundle/Entity/CheerleaderRepository.php
@@ -7,15 +7,19 @@ use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class CheerleaderRepository extends EntityRepository
 {
+    public function getAllQB()
+    {
+        return $this->getEntityManager()
+                    ->createQueryBuilder()
+                    ->select(['cl', 'p', 't'])
+                    ->from('SlashStudioAppBundle:Cheerleader', 'cl')
+                    ->leftJoin('cl.photo', 'p')
+                    ->leftJoin('cl.translations', 't')
+                    ->orderBy('t.name', 'ASC');
+    }
     public function getAll($maxResult = null)
     {
-        $qb = $this->getEntityManager()
-            ->createQueryBuilder()
-            ->select(['cl', 'p', 't'])
-            ->from('SlashStudioAppBundle:Cheerleader', 'cl')
-            ->leftJoin('cl.photo', 'p')
-            ->leftJoin('cl.translations', 't')
-            ->orderBy('t.name', 'ASC');
+        $qb = $this->getAllQB();
 
         return !empty($maxResult) ? new Paginator($qb->setMaxResults($maxResult)) : $qb->getQuery()->getResult();
     }
