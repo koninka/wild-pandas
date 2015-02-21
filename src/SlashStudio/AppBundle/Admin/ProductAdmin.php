b47f3c29b9b90c9033a7fda58cbdf6e1b3691030
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-21 22:07:38 +1000

    Добавляю ckeditor для товаров

--- src/SlashStudio/AppBundle/Admin/ProductAdmin.php
+++ src/SlashStudio/AppBundle/Admin/ProductAdmin.php
@@ -25,9 +25,17 @@ class ProductAdmin extends BaseAdmin
                                 'label' => 'show.label_name',
                                 'translation_domain' => $this->translationDomain,
                             ],
+                            'rawDescription' => ['display' => false],
+                            'descriptionFormatter' => ['display' => false],
                             'description' => [
                                 'label' => 'show.label_description',
                                 'translation_domain' => $this->translationDomain,
+                                'field_type' => 'sonata_formatter_type',
+                                'event_dispatcher' => $formMapper->getFormBuilder()->getEventDispatcher(),
+                                'format_field'   => 'descriptionFormatter',
+                                'source_field'   => 'rawDescription',
+                                'ckeditor_context' => 'default',
+                                'target_field'   => 'description',
                             ],
                         ],
                     ])
