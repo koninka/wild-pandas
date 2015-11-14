67887b3c46263371e0be1c232d95f55f9f7122e4
M. Tertishniy <mtertishniy@gmail.com>
2015-11-14 21:04:38 +1000

    Fix build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 php app/console cache:clear --env=dev
-php app/console doctrine:database:drop --force
-php app/console doctrine:database:create
-php app/console doctrine:schema:create
-php app/console doctrine:fixtures:load --fixtures=src/SlashStudio/AppBundle/DataFixtures/ORM
+:: php app/console doctrine:database:drop --force
+:: php app/console doctrine:database:create
+:: php app/console doctrine:schema:create
+:: php app/console doctrine:fixtures:load
 php app/console assets:install web --symlink
